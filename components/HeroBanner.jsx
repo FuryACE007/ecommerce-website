e2360d891515023063aabe6ee49d52cb17fb0657
FuryACE <sudhanshuvshekhar@gmail.com>
2022-07-06 18:11:18 +0530

    minor enhancements

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,19 +1,31 @@
 import React from "react";
+import Image from "next/image";
 
-import classes from '../styles/Home.module.css'
+import classes from "../styles/Home.module.css";
 
 const HeroBanner = () => {
   return (
-    <div
-      className={`bannerArea text-center mx-auto h-[21rem] shadow-[#C4FB6D] bg-[#A7D129] ${classes.card} rounded-3xl hover:cursor-pointer`}
-    >
-      <h1 className=" text-[#3E432E] mx-auto text-5xl lg:pt-10 font-semibold">
-        Best Selling Products
-      </h1>
-      <h3 className=" leading-loose text-xl text-[#616F39] font-medium">
-        The Gaming Store
-      </h3>
-    </div>
+    <>
+    <div className=" absolute rotate-12">
+        <Image
+          src="/headphone.png"
+          alt="hero-banner"
+          width={200}
+          height={200}
+          responsive
+        />
+      </div>
+      <div
+        className={`bannerArea text-center mx-auto md:mt-10 h-[21rem] shadow-[#C4FB6D] bg-[#A7D129] ${classes.card} rounded-3xl hover:cursor-pointer`}
+      >
+        <h1 className=" text-[#3E432E] mx-auto text-5xl lg:pt-10 font-semibold">
+          Best Selling Products
+        </h1>
+        <h3 className=" leading-loose text-xl text-[#616F39] font-medium">
+          Have them all in one place
+        </h3>
+      </div>
+    </>
   );
 };
 
