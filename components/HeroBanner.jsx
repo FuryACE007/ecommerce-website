1177a90b57d2c41601a53e958f2869e2b859e2b6
FuryACE <sudhanshuvshekhar@gmail.com>
2022-07-06 23:24:04 +0530

    added components

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,19 +6,18 @@ import classes from "../styles/Home.module.css";
 const HeroBanner = () => {
   return (
     <>
-    <div className=" absolute rotate-12">
+      <div className=" absolute rotate-12 md:w-[150px] md:left-14 md:top-8">
         <Image
           src="/headphone.png"
           alt="hero-banner"
           width={200}
           height={200}
-          responsive
         />
       </div>
       <div
         className={`bannerArea text-center mx-auto md:mt-10 h-[21rem] shadow-[#C4FB6D] bg-[#A7D129] ${classes.card} rounded-3xl hover:cursor-pointer`}
       >
-        <h1 className=" text-[#3E432E] mx-auto text-5xl lg:pt-10 font-semibold">
+        <h1 className=" text-[#3E432E] mx-auto text-5xl pt-10 font-semibold">
           Best Selling Products
         </h1>
         <h3 className=" leading-loose text-xl text-[#616F39] font-medium">
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = (props) => {
   return (
     <>
       Navbar
-      <div className="lg:px-[180px] md:px-[80px] sm:px-[30px] w-[90rem] min-h-screen mx-auto py-5">
+      <div className="lg:px-[180px] md:px-[80px] sm:px-[30px] lg:w-[90rem] min-h-screen mx-auto py-5">
         {props.children}
       </div>
       Footer
